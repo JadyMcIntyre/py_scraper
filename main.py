92887ce0e33fdce289208e8e8d6a3dd3b7821c60
Jady McIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2024-10-01 19:19:33 +0200

    clean content in main
    imported new methods into main
    present clean content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import scrape_website, extract_body_content, clean_body_content, split_dom_content
 
 st.title("AI web scraper")
 
@@ -7,5 +7,13 @@ url = st.text_input("Enter a website url: ")
 
 if st.button("Scrape site"):
     st.write("scraping the site")
+
     result = scrape_website(url)
-    print(result)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("View DOM content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
+    
