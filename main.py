197fe94506c9137a1d92fd00f6632f9226aaebd3
Jady McIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2024-10-01 19:48:07 +0200

    speak with llm
    capitalised message.
    added text box and button to talk to llm.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.title("AI web scraper")
 url = st.text_input("Enter a website url: ")
 
 if st.button("Scrape site"):
-    st.write("scraping the site")
+    st.write("Scraping the site...")
 
     result = scrape_website(url)
     body_content = extract_body_content(result)
@@ -16,4 +16,13 @@ if st.button("Scrape site"):
 
     with st.expander("View DOM content"):
         st.text_area("DOM Content", cleaned_content, height=300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse?")
+
+if st.button("Parse Content"):
+    if parse_description:
+        st.write("Parsing the content")
+
+        dom_chunks = split_dom_content(st.session_state.dom_content)
     
